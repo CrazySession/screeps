e8bb7199b97f1dc8f7cb3dfcaadc81f004d2e199
MrA <coding@highyieldgaming.com>
2018-01-29 08:08:20 +0100

    - repair creep

---
+++ role.handyman.js
@@ -0,0 +1,25 @@
+var roleHarvester = {
+
+    /** @param {Creep} creep **/
+
+    run: function(creep,sourceNum) {
+        //harvesting from source
+        if(creep.carry.energy < creep.carryCapacity) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[sourceNum]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[sourceNum]);
+            }
+        }else{
+            targets = creep.room.find(FIND_STRUCTURES, {
+                filter: object => object.hits < object.hitsMax});
+
+            targets.sort((a,b) => a.hits - b.hits);
+
+            if(targets.length > 0) {
+                if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
+            }
+        }
+
+}
